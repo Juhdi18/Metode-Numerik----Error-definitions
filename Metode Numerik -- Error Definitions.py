da33cf21d73bb6819a6b6a5783f8ce0aaa3833ed
Juhdi18 <150169684+Juhdi18@users.noreply.github.com>
2024-03-20 22:39:02 +0700

    Update Metode Numerik -- Error Definitions.py

--- Metode Numerik -- Error Definitions.py
+++ Metode Numerik -- Error Definitions.py
@@ -5,22 +5,24 @@ import matplotlib.pyplot as plt
 n = 3
 x = 0.5
 eksak = mt.e**x
-tollapprox = (0.5 * 10 **(2-n))
+tollapprox = (0.5 * 10 ** (2-n))
 epsT = np.zeros(25, dtype=float)
 epsA = np.zeros(25, dtype=float)
 hasil = np.zeros(25, dtype=float)
 
 for i in range(25):
     hasil[i] = (x**i)/mt.factorial(i) + hasil[i-1] if i != 0 else (x**i)/mt.factorial(i)
-    epsT[i] = abs((eksak - hasil[i]) / eksak)*100 
-    epsA[i] = abs((hasil[i] - hasil[i-1]) / hasil[i])*100 if i != 0 else 0
-    if i > 1 and epsA[i] < tollapprox:
-        break
+    epsT[i] = abs((eksak - hasil[i]) / eksak) * 100
+    epsA[i] = abs((hasil[i] - hasil[i-1]) / hasil[i]) * 100 if i != 0 else 0
+
     print("Term:", i+1)
     print("Hasil perkiraan:", hasil[i])
     print("Epsilon t:", epsT[i])
     print("Epsilon a:", epsA[i])
     print("--------------------")
+
+    if i > 1 and epsA[i] < tollapprox:
+        break
 
 # Plotting the results
 plt.figure(figsize=(12, 6))
